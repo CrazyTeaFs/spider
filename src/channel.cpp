8282401cbe7f30a3c1094832f4bf911a167524d9
frost <simple1115@gmail.com>
2015-05-18 18:49:24 +0800

    log error spell & on connect log success

--- src/channel.cpp
+++ src/channel.cpp
@@ -62,7 +62,7 @@ int Channel::SendRequest(const string &ip, int port, void *message, size_t len) 
 int Channel::SendResponse(SMessage *msg) {
 	int size = msg->ByteSize();
 	int length = size + sizeof(Header_t);
-	DEBUG("Dest %s:%d, Total Length To Send: %d Bytes, Header %d Bytes, Content:\n%s", iptostr(sk_->GetPeerAddr().sin_addr.s_addr), 
+	DEBUG("Destination %s:%d, Total Length To Send: %d Bytes, Header %d Bytes, Content:\n%s", iptostr(sk_->GetPeerAddr().sin_addr.s_addr), 
 	ntohs(sk_->GetPeerAddr().sin_port), length, sizeof(Header_t), msg->DebugString().c_str());
 
 	Header_t *h = (Header_t *)(sk_->GetWriteIndex());
