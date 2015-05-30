7e65ca0d500bfb4e2f2134ed6ef731dbab29cb11
frost <simple1115@gmail.com>
2015-05-30 23:10:48 +0800

    block io

--- base/thread_job.cpp
+++ base/thread_job.cpp
@@ -3,7 +3,12 @@
 using namespace std;
 	
 TJob::TJob():event_fd_(-1) {
-	event_fd_ = eventfd(O_NONBLOCK, 0);
+	// No Matter Block
+	event_fd_ = eventfd(0, 0);
+	if (event_fd_ < 0) {
+		CRIT("Cannot Open Eventfd");
+		exit(EXIT_FAILURE);
+	}
 	// Default Edge Triggle
 	EventDriver::Instance()->AddEventFd(event_fd_);
 }
